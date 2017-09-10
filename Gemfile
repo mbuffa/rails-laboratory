cad5b61dd351e39261d37a4dafb482c777825d4d
Maxime Buffa <mbuffa@gmail.com>
2017-09-10 23:49:44 +0200

    Install and initialize PhaserJS.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'redis-rails'
 
 gem 'sidekiq', '~> 4.0.0'
 
+gem 'phaser-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+;(function () {
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-scene', { preload: preload, create: create, update: update });
+
+  function preload() {
+    // console.log('preload');
+  }
+
+  function create() {
+    // console.log('create');
+  }
+
+  function update() {
+    // console.log('update');
+  }
+})();
